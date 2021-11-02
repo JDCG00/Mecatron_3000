59ef32ccb4e96825ad9e4a32eec078677a3d4f1a
JDCG00 <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2021-11-02 12:43:41 +0100

    v1

--- js/mecatron3000.js
+++ js/mecatron3000.js
@@ -18,16 +18,50 @@ class Juego{
         this.divPrincipal = null
         window.onload = this.iniciar.bind(this)
     }
-    iniciar(){
+    iniciar(){  
         console.log('Iniciando...');
         this.divPrincipal = document.getElementById('divPrincipal')
         this.vista.div = this.divPrincipal
-        this.generadorPalabras = window.setInterval(this.generarPalabra.bind(this), 1000)   
+        this.generadorPalabras = window.setInterval(this.generarPalabra.bind(this), 3000)   
         this.animador = window.setInterval(this.vista.moverPalabras.bind(this.vista), 100)    
+        window.onkeypress = this.pulsar.bind(this)
     }
     generarPalabra(){
         let nuevaPalabra = this.modelo.crearPalabra()
         this.vista.dibujar(nuevaPalabra)
+    }
+
+    pulsar(evento){
+        let letraPulsada = evento.key
+        console.log(`Has pulsado ${letraPulsada}`);
+        //Busco todas las palabras
+        let palabras = this.divPrincipal.querySelectorAll('.palabra')
+        
+        
+        for(let palabra of palabras){
+            let span = palabra.children.item(0)
+            let nodoTexto = palabra.childNodes[1]
+            let textoRestante = nodoTexto.nodeValue
+            let primeraLetraTextoRestante = textoRestante.charAt(0)
+            if (letraPulsada == primeraLetraTextoRestante) {
+                span.textContent += letraPulsada
+                nodoTexto.nodeValue = textoRestante.substring(1) 
+                
+                //Si ha completado la parabra, la elimina y suma puntos
+                if (nodoTexto.nodeValue.length == 0) {
+                    palabra.remove()
+                    /*this.modelo.sumarPunto()*/
+                }
+                else{
+                    
+                }
+
+            }
+            else{
+                //Ha fallado, repongo el texto de la palabra
+                
+            }
+        }
     }
 }
 class Vista{
@@ -42,6 +76,8 @@ class Vista{
         //<div class="palabra">MECA</div>
         let div = document.createElement('div')
         this.div.appendChild(div)
+        let span = document.createElement('span')
+        div.appendChild(span)
         div.appendChild(document.createTextNode(palabra))
         div.classList.add('palabra')
         div.style.top = '0px'
@@ -50,7 +86,7 @@ class Vista{
     moverPalabras(){
         //Busco todas las palabras del div
         let palabras = this.div.querySelectorAll('.palabra')
-        console.log(palabras);
+        //console.log(palabras);
         //Para cada palabra, aumenta su atributo top.
         for(let palabra of palabras){
             let top = parseInt(palabra.style.top)
